6442982c4011416b4b7594e02fc245b608062390
Jayesh Bhalla <jayeshbhalla891@gmail.com>
2017-05-14 09:50:16 +0000

    Set loop for iterating over infile's scanlines

--- whodunit/whodunit.c
+++ whodunit/whodunit.c
@@ -37,6 +37,20 @@ int main(int argc, char *argv[])
         return 3;
     }
 
+        // write outfile's BITMAPFILEHEADER
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    // write outfile's BITMAPINFOHEADER
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+    // determine padding for scanlines
+    int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // iterate over infile's scanlines
+    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    {
+        
+    }
     
     // success
     return 0;
